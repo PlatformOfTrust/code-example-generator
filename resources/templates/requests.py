971f8676be718bc882a8a037dcebca5a4edff435
Sven Laater <sven.laater@gmail.com>
2019-07-09 04:04:45 +0300

    fix: optional headers and conditional body/query

--- resources/templates/requests.py
+++ resources/templates/requests.py
@@ -2,8 +2,8 @@ import requests
 
 response = requests.{{request-method|name}}(
     '{{scheme}}://{{server-name}}{{uri}}',
-    {% if {query-string %}params={{query-string|safe}},{% endif %}
-    headers={{headers|json|safe|default:"{}"}},
+    {% if query-string %}params={{query-string|safe}},{% endif %}
+    {% if headers %}headers={{headers|json|safe|default:"{}"}},{% endif %}
     {% if body %}data=({{body|json|safe}}){% endif %}
 )
 
--- resources/templates/unirest.node.js
+++ resources/templates/unirest.node.js
@@ -2,8 +2,8 @@ const unirest = require("unirest");
 
 unirest
   .{{request-method|name}}("{{scheme}}://{{server-name}}{{uri}}")
-  .headers({{headers|json|safe|default:"{}"}})
-  {% if {query-string %}.query({{query-string|safe}}){% endif %}
+  {% if headers %}.headers({{headers|json|safe|default:"{}"}}){% endif %}
+  {% if query-string %}.query({{query-string|safe}}){% endif %}
   {% if body %}.send({{body|json|safe}}){% endif %}
   .then(({raw_body, status, code }) => {
     // output response to console as JSON
