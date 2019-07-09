439aa6ee7bbf9ae246e70fd2c89e1e269f431cd3
Sven Laater <sven.laater@gmail.com>
2019-07-09 04:58:03 +0300

    test(validate-args): improved tests

--- test/code_examples_generator/core_test.clj
+++ test/code_examples_generator/core_test.clj
@@ -3,25 +3,28 @@
    [clojure.test :refer :all]
    [clojure.string :as str]
    [clojure.tools.cli :refer [parse-opts]]
+   [code-examples-generator.test-utils :as u]
    [code-examples-generator.core :refer :all]))
 
 
-(deftest test-help
-  (testing "displaying help"
-    (are [args] (= (validate-args args) (:summary (parse-opts args cli-options)))
-      nil '() '("-h") '("--help"))))
-
-(deftest test-errors
-  (testing "passing incorrect parameters should result in error message"
-    (are [args] (str/starts-with? (validate-args args) "Unknown option:")
-      '("--blaah"))))
-
-(deftest test-version
-  (testing "version should be displayed"
-    (are [args] (= "v0.1.0" (validate-args args))
-      '("-v") '("--version") '("-v" "--help"))))
-
-(deftest test-main-method
+(deftest test-validate-args
+  (with-redefs [RAML->HTTP-examples (constantly "stub")]
+    (testing "displaying help"
+      (are [args] (= (validate-args args) (:summary (parse-opts args cli-options)))
+        nil '() '("-h") '("--help")))
+    (testing "version should be displayed"
+      (are [args] (= "v0.1.0" (validate-args args))
+        '("-v") '("--version") '("-v" "--help")))
+    (testing "missing or incorrect arguments should not trigger code generation"
+      (are [args] (not= "stub" (validate-args args))
+        '("--blaah") '("-s") '("-d")))
+    (testing "code example generation"
+      (are [args] (= "stub" (validate-args args))
+        (list (str "-s" (u/create-temp-dir)))
+        (list (str "-s" (u/create-temp-dir))
+              (str "-d" (u/uuid)))))))
+       
+(deftest test-main
   (testing "outputs the result of argument validation"
     (with-redefs
       [println (fn [s] s)
