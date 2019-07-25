af6884664a92db7b06ae141536f365e357eafc3d
Sven Laater <sven.laater@gmail.com>
2019-07-25 12:28:51 +0300

    fix: send payload as 'json' param (#27)

--- resources/templates/python.py
+++ resources/templates/python.py
@@ -4,7 +4,7 @@ response = requests.{{request-method|name}}(
     '{{scheme}}://{{server-name}}{{uri}}',
     {% if query-string %}params='{{query-string|safe}}',{% endif %}
     {% if headers %}headers={{headers|json|safe}},{% endif %}
-    {% if body %}data=({{body|json|safe}}){% endif %}
+    {% if body %}json=({{body|json|safe}}){% endif %}
 )
 
 json_response = response.json()
